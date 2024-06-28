e41ec5cb3147cf7d0c1ed08e222e1b16d685bfc2
MezdadAchour <m.achour2022@gmail.com>
2024-06-28 15:00:22 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // constructor et declaration de person
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: "Achour Mezdad",
+        bio: "Développeur web passionné avec 5 ans d'expérience, spécialisé en React et Node.js. Adepte des bonnes pratiques de code et de l'apprentissage continu. Amateur de randonnée et de photographie le week-end.",
+        imgSrc:
+          "https://st3.depositphotos.com/13194036/19088/i/450/depositphotos_190887154-stock-photo-young-hacker-developing-malware-dark.jpg",
+        profession: "Développeur Web",
+      },
+      show: true,
+    };
+  }
+
+  // Méthode pour mettre à jour l'état
+  toggleShow = () => {
+    this.setState((prevState) => ({ show: !prevState.show }));
+  };
+
+  render() {
+    const { person, show } = this.state;
+
+    return (
+      <div className="App">
+        <div className="card">
+          {/* si show = true */}
+          {show && (
+            <>
+              <img className="card-image" src={person.imgSrc} />
+              <div className="card-content">
+                <h1 className="card-name">{person.fullName}</h1>
+                <h2 className="card-profession">{person.profession}</h2>
+                <p className="card-bio">{person.bio}</p>
+              </div>
+            </>
+          )}
+          {/* boutton */}
+          <button className="toggle-button" onClick={this.toggleShow}>
+            {show ? "Cacher" : "Afficher"}
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
